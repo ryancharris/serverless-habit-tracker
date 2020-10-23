ad4bb95bda463360ec97f072fab6d28cc49456e9
Ryan Harris <harrisryan1@gmail.com>
2020-10-22 20:07:53 -0400

    parse sms and write values

--- handler.js
+++ handler.js
@@ -4,6 +4,7 @@ const faunadb = require("faunadb"),
   q = faunadb.query;
 
 module.exports.createByText = async event => {
+  // Instantiate client to write to FaunaDB
   const client = new faunadb.Client({
     secret: process.env.FAUNA_KEY,
     domain: "localhost",
@@ -11,12 +12,20 @@ module.exports.createByText = async event => {
     port: 8443
   });
 
+  // Parse values from SMS string using RegEx
+  const regex = new RegExp(
+    /(?<qty>[0-9\.]+)\s+(?<unit>[a-zA-Z]+).?\s+#(?<task>\w+)/gim
+  );
+  const parsedMessageArr = regex.exec(event);
+  const { qty, unit, task } = parsedMessageArr.groups;
+
+  // Write parsed SMS values to FaunaDB
   const res = await client.query(
-    q.Create(q.Collection("activity-log"), {
+    q.Create(q.Collection("log"), {
       data: {
-        timestamp: Date.now(),
-        activity: event.activity,
-        duration: event.duration
+        qty: qty,
+        task: task,
+        unit: unit
       }
     })
   );
